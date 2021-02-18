c88cf35add8fd48398ff56582cb2254bddc70352
culture-house <78912908+culture-house@users.noreply.github.com>
2021-02-18 22:23:32 +0100

    ep

---
+++ ep/data.js
@@ -0,0 +1,454 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vorplatz_neue-mitte",
+      "name": "Vorplatz_Neue Mitte",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07154148659421544,
+        "pitch": 0.04006974678358333,
+        "fov": 1.355520058834989
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07506264709375188,
+          "pitch": 0.18488939675124527,
+          "rotation": 0,
+          "target": "1-culture-house_zugang"
+        },
+        {
+          "yaw": -0.08114021753933898,
+          "pitch": -0.33977051687026716,
+          "rotation": 0,
+          "target": "5-blick-ber-die-dcher"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.06976438719223133,
+          "pitch": 0.0420188203849321,
+          "title": "Culture house<br>",
+          "text": "Ein Begegnungshaus soll in der neuen Mitte Stuttgarts entstehen und einen wertvollen Beitrag für die Bewohner- sowie Besucher der Stadt leisten. Angebote, die von- und für die Bevölkerung konzipiert werden, sollen aus den verschiedensten Kultur- und Themenbereichen ihren Raum finden und auf erlebnisreiche Art - zum Selbst-aktiv-werden, ebenso zum Austausch und zur Wissensbildung - vermittelt werden.<br>"
+        },
+        {
+          "yaw": -0.0690016158208806,
+          "pitch": 0.44087135334185135,
+          "title": "<div>Neue Mitte<br></div>",
+          "text": "Die Neue Mitte Stuttgarts wird zu einem neuen attraktiven, lebenswerteren Ort, der sich durch eine grünere- sowie lebenswertere <br><div>Stadtgestaltung mit vielfältigen Angeboten auszeichnet.</div><div><br></div><div>Eines davon, ist das neue Kulturareal Stuttgarts.<br></div>"
+        },
+        {
+          "yaw": -0.8623618310722208,
+          "pitch": 0.12360399984953574,
+          "title": "Die neue B14<br>",
+          "text": "<div>- verkehrsberuhigt</div><div>- grün</div><div>- familienfreundlich</div><div>- anziehend</div><div>- entspannender<br></div><div>- ruhiger<br></div>"
+        },
+        {
+          "yaw": 0.7022427616047242,
+          "pitch": 0.13771146491419195,
+          "title": "Die neue Esslingerstraße<br>",
+          "text": "<div>- verkehrsberuhigt</div><div>- grün</div><div>- Flanierqualität<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-culture-house_zugang",
+      "name": "Culture house_Zugang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.3770826689698623,
+        "pitch": 0.0347773830317184,
+        "fov": 1.5353512418857627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.014224697486884708,
+          "pitch": 0.1410905326668761,
+          "rotation": 0,
+          "target": "2-innenhof_ruheplatz"
+        },
+        {
+          "yaw": -0.011475959025041504,
+          "pitch": -0.461090354122053,
+          "rotation": 0,
+          "target": "5-blick-ber-die-dcher"
+        },
+        {
+          "yaw": 3.075954999214847,
+          "pitch": 0.08073226954593515,
+          "rotation": 0,
+          "target": "0-vorplatz_neue-mitte"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-innenhof_ruheplatz",
+      "name": "Innenhof_Ruheplatz",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35993497636594185,
+          "pitch": 0.06079840165793726,
+          "rotation": 0,
+          "target": "3-innenhof_ruheplatz-hinten"
+        },
+        {
+          "yaw": 0.7523356813309316,
+          "pitch": -0.15952877519943343,
+          "rotation": 0,
+          "target": "4-culture-house_cafe"
+        },
+        {
+          "yaw": 2.950865650887847,
+          "pitch": 0.1591296300903693,
+          "rotation": 0,
+          "target": "1-culture-house_zugang"
+        },
+        {
+          "yaw": 0.09892406894506856,
+          "pitch": -0.5696509468684141,
+          "rotation": 0,
+          "target": "5-blick-ber-die-dcher"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.019542340355375387,
+          "pitch": -0.00006608339599800672,
+          "title": "Ruhepol<br>",
+          "text": "<div>- Ein Platz zum Erholen und Zurückziehen -</div><div>Diesen findet man unterhalb der Kulturhäuser. Wasserbecken sorgen für eine entspannende Atmosphäre und schaffen zudem ein angenehmes Klima an heißen Sommertagen.<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-innenhof_ruheplatz-hinten",
+      "name": "Innenhof_Ruheplatz hinten",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3376854517888841,
+          "pitch": 0.0504699265756976,
+          "rotation": 0,
+          "target": "2-innenhof_ruheplatz"
+        },
+        {
+          "yaw": 0.6092200144475726,
+          "pitch": 0.011580787854178709,
+          "rotation": 0,
+          "target": "1-culture-house_zugang"
+        },
+        {
+          "yaw": -0.7884721426536281,
+          "pitch": -0.33041082776237474,
+          "rotation": 0,
+          "target": "4-culture-house_cafe"
+        },
+        {
+          "yaw": -0.0421407978649917,
+          "pitch": -0.620541367767137,
+          "rotation": 0,
+          "target": "5-blick-ber-die-dcher"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.33573452980310137,
+          "pitch": -0.38786260314029875,
+          "title": "Musik- und Veranstaltungsterrasse<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.7873246198191453,
+          "pitch": -0.3822526965226061,
+          "title": "Cafeterrasse<br>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-culture-house_cafe",
+      "name": "Culture house_Cafe",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3504756416905117,
+          "pitch": -0.42653693746136057,
+          "rotation": 0,
+          "target": "5-blick-ber-die-dcher"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.829699264819336,
+          "pitch": -0.35898178034901207,
+          "title": "Cafe - Culture house<br>",
+          "text": "<div>Dieses Cafe, geführt und gestaltet von Teilnehmern des Kulturhauses, wird zu einem attraktiven Treffpunkt für Bewohner des Viertels und weit darüber hinaus. Selbstgemachtes steht auf der Tagesordnung und wird von allen Besuchern geschätzt.</div><div><br></div><div>Frischer, angenehmer Pflanzenduft lässt das Cafe vorallem an heißen Sommertagen zu einem wohltuenden Ort werden.<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "5-blick-ber-die-dcher",
+      "name": "Blick über die Dächer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1324280942418259,
+          "pitch": 0.20658002968120925,
+          "rotation": 0,
+          "target": "0-vorplatz_neue-mitte"
+        },
+        {
+          "yaw": 0.861785422478011,
+          "pitch": 1.241379024346216,
+          "rotation": 0,
+          "target": "3-innenhof_ruheplatz-hinten"
+        },
+        {
+          "yaw": -0.1149508802047805,
+          "pitch": 0.9374205403811366,
+          "rotation": 0,
+          "target": "2-innenhof_ruheplatz"
+        },
+        {
+          "yaw": -0.6947780293557351,
+          "pitch": 1.2648890658892853,
+          "rotation": 0,
+          "target": "4-culture-house_cafe"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.21302172529452434,
+          "pitch": 0.8797402395708538,
+          "title": "Begrünter Innenhof<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.09739468296540466,
+          "pitch": 0.7789602078631486,
+          "title": "Musik- und Veranstaltungsterrasse<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.4702282889501035,
+          "pitch": 0.6931385457408119,
+          "title": "Laubengang<br>",
+          "text": "Laubengänge vermitteln Kommunikation, einfache Erschließungswege sowie spannende Blicke. Zudem ermöglichen sie das Bewahren wertvoller Nutzflächen, ohne ein Durchqueren langgestreckter Flure ermöglichen zu müssen.&nbsp; <br>"
+        },
+        {
+          "yaw": -0.46768370401805015,
+          "pitch": 1.3035492020908652,
+          "title": "Cafeterrasse",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5179371547704505,
+          "pitch": 0.2808312726446953,
+          "title": "<div>Nutzungseinheit</div><div>Urban Gardening<br></div>",
+          "text": "Diese Einheit befasst sich mit dem Thema Urban Gardening. Anhand von Workshops und Veranstaltungen wird auf interaktive Art allen Interessierten gezeigt, wie wir mit unserer Umwelt umgehen sollten. <br>"
+        },
+        {
+          "yaw": -0.5828079925664191,
+          "pitch": 0.4912025346065434,
+          "title": "<div>Urban Gardening</div><div>Gemüseterrasse<br></div>",
+          "text": "<div>Jede Nutzungseinheit erhält eine Terrasse oder sogenannte Flex-Spaces um aktiv Interessierten zu zeigen, wie man wie hier in diesem links aufgeführten Beispiel grüner, gesünder und demzufolge besser leben kann.<br>&nbsp; </div>"
+        },
+        {
+          "yaw": -0.2771347339822121,
+          "pitch": 0.31562975919214864,
+          "title": "<div>Urban Gardening</div><div>Kräuterterrasse<br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.13325007409954637,
+          "pitch": 0.4034430338505324,
+          "title": "Gewächshaus",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.5785736191014372,
+          "pitch": 0.7744286707090762,
+          "title": "<div>Urban Gardening</div><div>Blumenterrasse<br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.3713908476909573,
+          "pitch": 0.21851871030705894,
+          "title": "Erschließungshaus<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.7154870851496895,
+          "pitch": 0.38452506703722733,
+          "title": "<div>Nutzungseinheit</div><div>Veranstaltungshaus</div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.251463646483675,
+          "pitch": 0.4237858784934101,
+          "title": "Erschließungshaus<br>",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.1167921270901946,
+          "pitch": 0.48545477667768644,
+          "title": "<div>Nutzungseinheit</div><div>Kursräume Umwelt<br></div>",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.0517943819663369,
+          "pitch": 0.20699318802060063,
+          "title": "<div>Architektur</div><div>Was soll entstehen ?<br></div>",
+          "text": "<div>Ein Haus, das aus vielen kleinen Einheiten mit einem wiederholenden Modul von 10x10m zusammengesetzt ist, soll entlang der neuen, grünen B14 entstehen.</div><div><br></div><div>Zehn Einheiten, die je einer Nutzung zugeordnet sind, dennoch miteinander in Verbindung stehen, machen das Areal zu einem vielfältig bespielten Ort.</div><div><br></div><div><br>Flexibilität, die aus der Grundstruktur hervorgeht und demzufolge Aneignung verspricht, lässt Veränderungen für neue Nutzungen zu.<br></div><div><br></div>"
+        },
+        {
+          "yaw": 1.7447460829206163,
+          "pitch": 0.5722755442456844,
+          "title": "<div>Nutzungseinheit</div><div>Weiterbildungshaus<br></div>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
